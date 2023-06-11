b9ebd0529b4885a95b8686565b71d778e454d6b0
Aayaan <66046533+AayaanN@users.noreply.github.com>
2023-06-11 14:28:51 -0400

    Started on creating add stock, stock components

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useState} from "react"
+import {Stocks} from './components/stocks'
 
 function App() {
 
+  const[stocks, set_stocks] = useState([])
+
+  const[random_temp, set_random_temp] = useState(0)
+
+  // const submitTemp = () => {
+
+  //   fetch('/add_data', {
+  //     method: 'POST',
+  //     headers: {'Content-Type': 'application/json'},
+  //   }).then( () => {
+  //     console.log('temp added')
+  //   })
+  // }
+
+
   useEffect(() => {
-    fetch('/add_data').then(
-      response => response.json().then(
-        data => {
-          console.log(data);
-        }
-      )
-    )
-  }
-
-
-  , []) 
+    fetch("/get_data").then(response =>
+      response.json().then(data => {
+        set_stocks(data);
+      })
+    );
+  }, []);
 
 
   return (
     <div className="App">
+
+      <Stocks>stocks={stocks}</Stocks>
 
     </div>
   );
---
+++ react_frontend/src/components/Stocks.js
@@ -0,0 +1,8 @@
+import React, {useState} from 'react'
+import {Button} from 'semantic-ui-react'
+
+export const Stocks = ({stocks}) => {
+
+    return <div>{stocks.length}</div>
+    
+}
