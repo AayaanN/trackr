03e3f51d01492a35df87d84300c1e70e8a778eff
Aayaan <66046533+AayaanN@users.noreply.github.com>
2023-07-26 22:53:27 -0400

    played around with news article formatting

--- react_frontend/src/components/news_article.js
+++ react_frontend/src/components/news_article.js
@@ -10,13 +10,13 @@ const NewsComponent = ({selected_stock}) => {
   useEffect(() => {
 
     addNews();
-    console.log('just ran addNews')
+    // console.log('just ran addNews')
     fetch(`/get_article?selected_stock=${selected_stock}`).then(
     // fetch('/get_article').then(
       response => response.json().then(
         data => {
           setNews(data.news_article)
-          // console.log( data.stock_name)
+          // console.log('time', data)
         }
       )
     )
@@ -49,15 +49,15 @@ const NewsComponent = ({selected_stock}) => {
     <div className='w-full'>
       
       <a href={news.url} target="_blank" rel="noopener noreferrer" >
-        <div className='flex neumorphic-shadow rounded-xl bg-gray-900 w-4/5 h-60 mx-40 hover:bg-[#0B1019] '>
-          <img src={news.image} className='rounded-xl h-52 w-52 object-cover m-4'></img>
+        <div className='flex neumorphic-shadow rounded-xl bg-gray-900 w-4/5 h-48 mx-40 hover:bg-[#0B1019] '>
+          <img src={news.image} className='rounded-xl h-40 w-40 shadow-lg shadow-gray-950 object-cover m-4'></img>
           <div className='flex flex-col m-3'>
-            <div>
-              <p className='text-white font-bold text-2xl'>{news.publisher_name}</p>
-              <p className='text-white font-bold text-2xl'>{news.time}</p>
+            <div className='flex'>
+              <p className='text-blue-600 font-black text-2xl mr-8 w-68'>{news.publisher_name}</p>
+              <p className='text-blue-400 font-bold text-2xl'>{news.time}</p>
             </div>
     
-            <p className='text-white font-semibold text-2xl'>{news.article_name}</p>
+            <p className='text-white font-semibold text-2xl m-3'>{news.article_name}</p>
           </div>
           
         </div>
