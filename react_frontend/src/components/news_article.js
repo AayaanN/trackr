bf067ae0b6409758bebfd3d353a60f524278ad47
Aayaan <66046533+AayaanN@users.noreply.github.com>
2023-08-03 03:06:47 -0400

    Fixed news css wrt to graph

--- react_frontend/src/components/news_article.js
+++ react_frontend/src/components/news_article.js
@@ -46,18 +46,18 @@ const NewsComponent = ({selected_stock}) => {
 
 
   return (
-    <div className='w-full'>
+    <div className='w-full mt-8'>
       
       <a href={news.url} target="_blank" rel="noopener noreferrer" >
-        <div className='flex neumorphic-shadow rounded-xl bg-gray-900 w-4/5 h-48 mx-40 hover:bg-[#0B1019] '>
-          <img src={news.image} className='rounded-xl h-40 w-40 shadow-lg shadow-gray-950 object-cover m-4'></img>
+        <div className='flex neumorphic-shadow rounded-xl bg-gray-900 w-5/6 h-24 mx-24 hover:bg-[#0B1019] '>
+          <img src={news.image} className='rounded-xl h-16 w-16 shadow-lg shadow-gray-950 object-cover m-4'></img>
           <div className='flex flex-col m-3'>
             <div className='flex'>
-              <p className='text-blue-600 font-black text-2xl mr-8 w-68'>{news.publisher_name}</p>
-              <p className='text-blue-400 font-bold text-2xl'>{news.time}</p>
+              <p className='text-blue-600 font-semibold text-lg mr-8 w-68'>{news.publisher_name}</p>
+              <p className='text-blue-400 font-normal text-lg'>{news.time}</p>
             </div>
     
-            <p className='text-white font-semibold text-2xl m-3'>{news.article_name}</p>
+            <p className='text-white font-medium text-lg'>{news.article_name}</p>
           </div>
           
         </div>
